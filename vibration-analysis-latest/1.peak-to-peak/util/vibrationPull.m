dc44dea789e475f3ce62c4016ecd62f885397781
krajoff <t0k@bk.ru>
2023-03-31 16:17:07 +0300

    Fix same bugs

--- vibration-analysis-latest/1.peak-to-peak/util/vibrationPull.m
+++ vibration-analysis-latest/1.peak-to-peak/util/vibrationPull.m
@@ -6,33 +6,29 @@ function opt = vibrationPull(opt)
     PairSignal = opt.Pairs;
     sgn = 1;
     pathfile = @(x) char(fullfile(opt.GeneralFolder, folder(x)));
+    tic
     for i = 1:length(NameTable)
         ref = sgn;
-        % Return shaft vibration
-        if contains(NameTable{i}, 'haft')
-            for res = 1:PairSignal(i)
+        for res = 1:PairSignal(i)
+            % Return shaft vibration
+            if contains(NameTable{i}, 'haft')
                 opt.Channels{ref} = shAn(exfile, pathfile(ref), ...
                     opt, opt.OutlierFilter(i), char(folder(ref)), NaN);                      
                 ref = ref+1;
-            end   
-            opt.(NameTable{i}) = join2table(opt.Channels(sgn:sgn+PairSignal(i)-1));
-        elseif contains(NameTable{i}, 'ore')
-           % Return core vibration
-           for res = 1:PairSignal(i)
+            elseif contains(NameTable{i}, 'ore')
+            % Return core vibration
                opt.Channels{ref} = scAn(exfile, pathfile(ref), opt, NaN);                         
                ref = ref+1;
-           end   
-           opt.(NameTable{i}) = join2table(opt.Channels(sgn:sgn+PairSignal(i)-1));
-        else
+            else
             % Return bearing vibration
-            for res = 1:PairSignal(i)
                 opt.Channels{ref} = vibrAn(exfile, pathfile(ref), opt, NaN);                      
                 ref = ref+1;
-            end   
-            opt.(NameTable{i}) = join2table(opt.Channels(sgn:sgn+PairSignal(i)-1));  
+            end
         end
+        opt.(NameTable{i}) = join2table(opt.Channels(sgn:sgn+PairSignal(i)-1));  
         fprint(NameTable{i}, sgn, sgn+PairSignal(i)-1);
         sgn = sgn + PairSignal(i);
     end
+    toc
     save([opt.StationHPP,'.mat'], 'opt');
 end
