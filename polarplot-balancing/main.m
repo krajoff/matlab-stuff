e359e319bb1f5f380bb2e81b95f893d6bd961396
krajoff <t0k@bk.ru>
2023-05-31 15:23:14 +0300

    Balancing modification

--- polarplot-balancing/main.m
+++ polarplot-balancing/main.m
@@ -23,4 +23,4 @@ end
 
 ax = gca;
 %rlim([0.95*min(T.DoubleAmp) 1.05*max(T.DoubleAmp)])
-thetalim([120 210])
+%thetalim([120 210])
---
+++ polarplot-balancing/util/getcomplex.m
@@ -0,0 +1,3 @@
+function result = getcomplex(amp,phase)
+    result = amp*exp(1j*deg2rad(phase));
+end
