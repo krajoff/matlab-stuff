aac84038f26850e4746faf7dd1e66dd9faff383e
krajoff <iioz@ya.ru>
2024-01-05 21:46:52 +0300

    Add ISO 7919-5

--- voltage-recovery/VoltageRecovery.m
+++ voltage-recovery/VoltageRecovery.m
@@ -8,7 +8,7 @@ opt = options_i170A;
 mfilepath = fullfile(pwd, 'data\mat', opt.name);
 load(mfilepath);
 data = decreaseStep(data, opt); 
-
+data = includeNil(data);
 voltageCalculation(data, -data.VoltageA+data.VoltageB, opt, "Voltage AB");
 voltageCalculation(data, -data.VoltageC+data.VoltageB, opt, "Voltage BC");
 voltageCalculation(data, data.VoltageC-data.VoltageA, opt, "Voltage CA");
