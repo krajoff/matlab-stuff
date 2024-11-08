9616927522a2711bcef00a4d638d93c2be011cb5
krajoff <t0k@bk.ru>
2024-11-08 13:47:50 +0300

    Small full data

--- sudden-short-circuit/util/readCSV.m
+++ sudden-short-circuit/util/readCSV.m
@@ -1,6 +1,6 @@
 function data = readCSV(filepath)
     data = readtable(filepath);
     variableName = {'Time', 'VoltageA', 'VoltageB', 'VoltageC', ...
-        'CurrentA', 'CurrentB', 'CurrentC', 'CurrentC-Fluke'};
+        'CurrentA', 'CurrentB', 'CurrentC'};
     data.Properties.VariableNames = variableName;
 end
